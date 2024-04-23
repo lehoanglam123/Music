14693ea7ff3a20f69066aafbe618a89a10ad1f45
lehoanglam123 <lehoanglamit@gmail.com>
2024-04-23 17:01:46 +0700

    create class appconfig

---
+++ music/src/main/java/com/spring/security/config/jwt/ApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.spring.security.config.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    
+}
